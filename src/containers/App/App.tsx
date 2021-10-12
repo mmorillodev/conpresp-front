98f6871195050a61e6c39bacf4e27f99ee033250
lamatheus.g <m.goncalves@trustetc.com>
2021-10-11 22:41:08 -0300

    Revert "replace relative paths with module aliases"
    This reverts commit 7e0773a7414a453fcefa179b4002d856924f5a4b.

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route } from 'react-router-dom'
-import Login from '@pages/Login/Login'
+import Login from '../../pages/Login/Login'
 import styles from './App.module.scss'
 
 const App = () => (
