f02482dd3ad5474122f85aeba96ac761470ae48a
lamatheus.g <m.goncalves@trustetc.com>
2021-10-15 18:42:57 -0300

    create snapshot test as example

--- src/containers/App/App.test.tsx
+++ src/containers/App/App.test.tsx
@@ -1,6 +1,9 @@
-import { render } from '@testing-library/react'
+import renderer from 'react-test-renderer'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
+describe('<App />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
