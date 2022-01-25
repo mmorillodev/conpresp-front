d6e992fd092ee7d0220047fe936763e91e44050e
Matheus Morillo <matheus.morillo2000@gmail.com>
2022-01-24 21:19:44 -0300

    Create Tombamento and Dados Gerais sections

--- src/pages/NewPatrimony/NewPatrimony.tsx
+++ src/pages/NewPatrimony/NewPatrimony.tsx
@@ -16,46 +16,105 @@ const NewPatrimony: FC = () => (
     </section>
     <form>
       <section>
-        <h2>Responsável Técnico</h2>
-        <span>
-          Informações básicas do técnico responsável pelo preenchimento
-        </span>
+        <div className={styles.sectionTitle}>
+          <h2>Responsável Técnico</h2>
+          <span>
+            Informações básicas do técnico responsável pelo preenchimento
+          </span>
+        </div>
         <label>
-          <span>Responsável pelo preenchimento</span>
-          <input disabled />
+          <h4>Responsável pelo preenchimento</h4>
+          <input disabled value="Matheus Morillo" />
         </label>
         <label>
-          <span>Grupo</span>
-          <input disabled />
+          <h4>Grupo</h4>
+          <select disabled>
+            <option>Grupo UAM</option>
+            <option>Conpresp</option>
+            <option>DHP</option>
+          </select>
         </label>
       </section>
       <section>
-        <h2>Dados Gerais</h2>
-        <span>Dados gerais do tombamento</span>
-        <label>
-          <span>Item na resolução</span>
-          <input />
-        </label>
-        <label>
-          <span>Denominação</span>
-          <input />
-        </label>
-        <label>
-          <span>Classificação</span>
-          <input />
-        </label>
-        <label>
-          <span>Propriedade</span>
-          <input />
-        </label>
-        <label>
-          <span>Uso atual</span>
-          <input />
-        </label>
-        <label>
-          <span>Uso original</span>
-          <input />
-        </label>
+        <div className={styles.sectionTitle}>
+          <h2>Dados Gerais</h2>
+          <span>Dados gerais do tombamento</span>
+        </div>
+        <div className={styles.flexbox}>
+          <label style={{ flexBasis: '149px', flexGrow: 0 }}>
+            <h4>Item na resolução</h4>
+            <input placeholder="Número do bem" />
+          </label>
+          <label>
+            <h4>Denominação</h4>
+            <input placeholder="Nome do bem" />
+          </label>
+        </div>
+        <div className={styles.flexbox}>
+          <label>
+            <h4>Classificação</h4>
+            <input placeholder="Label" />
+          </label>
+          <label>
+            <h4>Propriedade</h4>
+            <input placeholder="Label" />
+          </label>
+        </div>
+        <div className={styles.flexbox}>
+          <label>
+            <h4>Uso atual</h4>
+            <input placeholder="Label" />
+          </label>
+          <label>
+            <h4>Uso original</h4>
+            <input placeholder="Label" />
+          </label>
+        </div>
+      </section>
+      <section>
+        <div className={styles.sectionTitle}>
+          <h2>Tombamento</h2>
+          <span>Dados gerais do tombamento</span>
+        </div>
+        <div className={styles.resolutionItem}>
+          <div className={styles.grayBgTitle}>Conpresp</div>
+          <div className={styles.resolutionItemDesc}>
+            <div style={{ flex: 3 }}>
+              <h4>Resolução</h4>
+              <input placeholder="Label" />
+            </div>
+            <div>
+              <h4>Ano do Tombamento</h4>
+              <input placeholder="Label" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.resolutionItem}>
+          <div className={styles.grayBgTitle}>Condepaat</div>
+          <div className={styles.resolutionItemDesc}>
+            <div style={{ flex: 3 }}>
+              <h4>Resolução</h4>
+              <input placeholder="Label" />
+            </div>
+            <div>
+              <h4>Ano do Tombamento</h4>
+              <input placeholder="Label" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.resolutionItem}>
+          <div className={styles.grayBgTitle}>Iphan</div>
+          <div className={styles.resolutionItemDesc}>
+            <div style={{ flex: 3 }}>
+              <h4>Resolução</h4>
+              <input placeholder="Label" />
+            </div>
+            <div>
+              <h4>Ano do Tombamento</h4>
+              <input placeholder="Label" />
+            </div>
+          </div>
+        </div>
       </section>
     </form>
   </>
